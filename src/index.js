38f25e2d15d2717157da3003adcdd9fa9add2d41
Maksim <maxkov112211@gmail.com>
2021-03-12 19:55:53 +0300

    inti:solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-module.exports = function toReadable (number) {
-  
+let ones = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
+let tens = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+let teens = [
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+
+function convert_hundreds(num) {
+    
+        if (num > 99) {
+            if (num % 100 != 0)
+                return (
+                    ones[Math.floor(num / 100)] + " hundred " + convert_tens(num % 100)
+                );
+            else return ones[Math.floor(num / 100)] + " hundred";
+        } else return convert_tens(num);
+
 }
+
+function convert_tens(num) {
+    if (num < 10) return ones[num];
+    else if (num >= 10 && num < 20) {
+        return teens[Math.floor(num % 10)];
+    } else if (num % 10 != 0) {
+        return tens[Math.floor(num / 10)] + " " + convert_ones(num % 10);
+    } else return tens[Math.floor(num / 10)];
+}
+
+function convert_ones(num) {
+    if (num > 0) return ones[num];
+    else return;
+}
+
+module.exports = function toReadable(number) {
+    if (number == 0) return ones[0];
+    return convert_hundreds(number);
+};
